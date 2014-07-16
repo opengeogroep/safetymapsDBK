4c295ffdb6c50973d8a2eee6204d34f2a8c7ac9a
Geert Plaisier <geert.plaisier@gnamic.nl>
2014-07-16 17:56:16 +0200

    Moved DBK title down (bottom, right). Updated size calculation and added resize event to calculate width on

--- public/js/dbkjs/dbkjs.js
+++ public/js/dbkjs/dbkjs.js
@@ -414,18 +414,22 @@ $(document).ready(function() {
         $(dbkjs).bind('dbkjs_init_complete', function() {
             FastClick.attach(document.body);
             (function() {
+                var timer;
+                function throttleCalc() {
+                    window.clearTimeout(timer);
+                    timer = window.setTimeout(calcMaxWidth, 150);
+                }
                 function calcMaxWidth() {
                     // Calculate the max width for dbk title so other buttons are never pushed down when name is too long
-                    var childWidth = 0;
-                    $('.main-button-group .btn-group').each(function() {
-                        childWidth += $(this).outerWidth();
-                    });
-                    var maxWidth = $('.main-button-group').outerWidth() - childWidth;
-                    $('.dbk-title').css('max-width', (maxWidth - 25) + 'px');
+                    var maxWidth = $('body').outerWidth();
+                    $('.dbk-title').css('max-width', (maxWidth - 70) + 'px');
                 }
                 // Listen for orientation changes
                 window.addEventListener("orientationchange", function() {
                     calcMaxWidth();
+                }, false);
+                window.addEventListener("resize", function() {
+                    throttleCalc();
                 }, false);
                 calcMaxWidth();
             }());
