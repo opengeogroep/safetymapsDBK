93cbb1397fd1ab94c087bc37e8dbca11d6c97bf6
Matthijs Laan <matthijsln@gmail.com>
2014-11-13 17:50:38 +0100

    connectionmonitor module toegevoegd

---
+++ public/js/dbkjs/modules/connectionmonitor.js
@@ -0,0 +1,62 @@
+/*!
+ *  Copyright (c) 2014 B3Partners (info@b3partners.nl)
+ *
+ *  This file is part of safetymapDBK
+ *
+ *  safetymapDBK is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  safetymapDBK is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with safetymapDBK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+var dbkjs = dbkjs || {};
+window.dbkjs = dbkjs;
+dbkjs.modules = dbkjs.modules || {};
+dbkjs.modules.connectionmonitor = {
+    id: "dbk.module.connectionmonitor",
+    connected: null,
+    debug: null,
+    register: function(options) {
+        this.debug = !!dbkjs.options.connectionmonitorDebug;
+
+        this.connected = true;
+
+        $(".main-button-group").append($("<div class=\"btn-group pull-left connection-btn-group\">" +
+            "<a id=\"connection\" href=\"#\" title=\"Verbindingsstatus\" class=\"btn navbar-btn btn-default\">" +
+            "<i id=\"connectionicon\" class=\"icon-signal\" style=\"color: green\"></i></a>"));
+
+        $("#connection").click(function() {
+            window.location.href = $('#permalink').attr('href');
+        });
+
+        OpenLayers.IMAGE_RELOAD_ATTEMPTS = 10;
+
+        OpenLayers.Util.onImageLoadError = function() {
+            dbkjs.modules.connectionmonitor.onConnectionError();
+        };
+    },
+    onConnectionError: function() {
+        this.connected = false;
+
+        $("#connectionicon").attr("class", "icon-exclamation-sign");
+        $("#connectionicon").attr("style", "color: red");
+    },
+    onConnectionOK: function() {
+        console.log("connection ok");
+        if(!this.connected) {
+            $("#connectionicon").attr("class", "icon-refresh");
+            $("#connectionicon").attr("style", "color: green");
+        }
+        this.connected = true;
+    }
+};
+
