aa6fb2c055cd07e1e11c3031a66c909ed700749f
Matthijs Laan <matthijsln@gmail.com>
2018-02-02 16:28:51 +0100

    adres zoektab default

--- nodeless/overrides/public/search.js
+++ nodeless/overrides/public/search.js
@@ -3,15 +3,16 @@ dbkjs.modules.search.viewmode = 'fullscreen';
 
 dbkjs.modules.search.createSearchGroup = function() {
     var search_group = $('<div></div>').addClass('input-group input-group-lg');
-    var search_input = $('<input id="search_input" name="search_input" autocomplete="off" type="text" class="form-control" placeholder="' + i18n.t("search.dbkplaceholder") + '">');
+    var search_input = $('<input id="search_input" name="search_input" autocomplete="off" type="text" class="form-control" placeholder="' + i18n.t("search.addressplaceholder") + '">');
 
     if(dbkjs.options.searchTabs) {
         var search_tabs = $(
             '<ul id="search_tabs" class="nav nav-pills" style="margin-bottom: 10px">' +
-                    '<li id="search_li_dbk" class="active"><a data-toggle="tab" href="#" id="s_dbk"><i class="fa fa-building"></i> ' + i18n.t("search.dbk") + '</a></li>' +
+                    '<li id="search_li_dbk"><a data-toggle="tab" href="#" id="s_dbk"><i class="fa fa-building"></i> ' + i18n.t("search.dbk") + '</a></li>' +
                     '<li id="search_li_oms"><a  data-toggle="tab" href="#" id="s_oms"><i class="fa fa-bell"></i> ' + i18n.t("search.oms") + '</a></li>' +
                     '<li id="search_li_library" style="display: none"><a  data-toggle="tab" href="#" id="s_library"><i class="fa fa-book"></i> ' + i18n.t("search.library") + '</a></li>' +
-                    '<li id="search_li_wo" style="display: ' + (dbkjs.modules.waterongevallen.features && dbkjs.modules.waterongevallen.features.length > 0 ? "block" : "none") + '"><a data-toggle="tab" href="#" id="s_wo"><i class="fa fa-life-buoy"></i> ' + i18n.t("search.wo") + '</a></li>' +                     '<li id="search_li_address"><a  data-toggle="tab" href="#" id="s_address"><i class="fa fa-home"></i> ' + i18n.t("search.address") + '</a></li>' +
+                    '<li id="search_li_wo" style="display: ' + (dbkjs.modules.waterongevallen.features && dbkjs.modules.waterongevallen.features.length > 0 ? "block" : "none") + '"><a data-toggle="tab" href="#" id="s_wo"><i class="fa fa-life-buoy"></i> ' + i18n.t("search.wo") + '</a></li>' +
+                    '<li id="search_li_address" class="active"><a  data-toggle="tab" href="#" id="s_address"><i class="fa fa-home"></i> ' + i18n.t("search.address") + '</a></li>' +
                 '</ul>'
         );
 
